713aea2a12db41c568e7e3fd58269b425e35270d
sangmin yi <ssanay79@gmail.com>
2020-11-01 20:22:18 +0900

    add new fragment

--- app/src/main/java/com/example/drian/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/drian/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.drian.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -14,6 +16,8 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
+    // private val client = OkHttpClient()
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -22,10 +26,14 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val btn: Button = root.findViewById(R.id.btn)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
         return root
     }
+
+
+
 }
